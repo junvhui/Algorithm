4b0382e300ae51337650ca43b05a736ff594b828
Junhui Lee <dev.junhui@gmail.com>
2023-02-25 02:49:58 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,4 +1,10 @@
 function solution(my_string) {
-    return my_string.match(/\d/g).sort((a, b) => a - b).map(n => Number(n));
+    let str = my_string.split('');
+    const answer = [];
+    for (let i = 0; i < str.length; i++) {
+        if (!isNaN(str[i])) {
+            answer.push(Number(str[i]))
+        }
+    }
+    return answer.sort((a, b) => a - b);
 }
-
