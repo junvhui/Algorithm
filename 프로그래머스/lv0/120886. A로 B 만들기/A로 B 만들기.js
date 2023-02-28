d2cccfa357acc1a720fef4a88aa71a555dc63202
Junhui Lee <dev.junhui@gmail.com>
2023-03-01 00:11:14 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,6 +1,3 @@
 function solution(before, after) {
-
-    before = [...before].sort()
-    after = [...after].sort()
-    return before.filter((a,i) => a === after[i]).length === after.length ? 1 : 0
+    return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0;
 }
