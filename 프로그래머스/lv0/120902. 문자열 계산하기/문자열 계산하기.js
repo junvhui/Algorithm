064f6d6ed8911f8d15dc30ba03ad3fa5bf8c4710
Junhui Lee <dev.junhui@gmail.com>
2023-03-11 20:33:22 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,14 +1,14 @@
 function solution(my_string) {
-
+    
     var split = my_string.split(" ")
-    var num = split[0] * 1;
+    var num1 = split[0] * 1;
     for(let i = 1; i < split.length; i++){
         if(split[i] === "+"){
-           num += split[i+1] * 1;
+           num1 += split[i+1] * 1;
         }else if(split[i] === "-"){
-           num -= split[i+1] * 1;
+           num1 -= split[i+1] * 1;
         }
     }
 
-    return num;
+    return num1;
 }
