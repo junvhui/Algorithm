461f21badf90042091c34428f0718cd01d892268
Junhui Lee <dev.junhui@gmail.com>
2023-03-31 01:40:35 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -2,9 +2,6 @@ function solution(n)
 {
     var answer = 0;
     let s = n.toString().split("")
-    for(let i = 0; i < s.length; i++){
-        answer += parseInt(s[i])
-    }
 
-    return answer;
+    return s.reduce((a, b) => a + parseInt(b), 0);
 }
