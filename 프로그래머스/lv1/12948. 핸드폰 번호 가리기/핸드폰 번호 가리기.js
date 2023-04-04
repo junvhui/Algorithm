8dfe290df501fe670d638861512d374090b446a0
Junhui Lee <dev.junhui@gmail.com>
2023-04-04 23:38:35 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -3,8 +3,5 @@ function solution(phone_number) {
     for(let i = 0; i < phone_number.length - 4; i++){
         answer.push("*");
     }
-    for(let i = phone_number.length - 4; i < phone_number.length; i++){
-        answer.push(phone_number[i])
-    }
-    return answer.join("");
+    return answer.join("") + phone_number.slice(-4);
 }
