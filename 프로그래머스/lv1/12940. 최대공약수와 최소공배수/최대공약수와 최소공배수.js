eea85fa20517598c0047c3d5d0932a075363afbb
Junhui Lee <dev.junhui@gmail.com>
2023-04-07 03:42:13 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,23 @@
+function solution(n, m) {
+    var answer = [];
+    let max = 0;
+    let min = 0;
+    if(n < m){
+        max = m;
+        min = n;
+    }else{
+        max = n;
+        min = m;
+    }
+    for(let i = max; i >= 1; i--){
+        if(n % i == 0 && m % i == 0){
+            answer.push(i)
+            break;
+        }
+    }
+    
+    answer.push(n*m/answer[0])
+    
+    
+    return answer;
+}
