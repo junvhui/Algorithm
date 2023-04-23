b75695a9bd212ff264836a7853e9c9c44a0120d6
Junhui Lee <dev.junhui@gmail.com>
2023-04-23 20:19:38 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,20 +1,20 @@
 function solution(num) {
-    var count = 0;
+    var cnt = 0;
     if(num == 1){
         return 0;
     }
     for(let i = 1; i <= 500; i++){
         if(num % 2 == 0){
             num = num/2
-            count++
+            cnt++
             if(num == 1){
-                return count
+                return cnt
             }
         }else{
             num = num * 3 + 1
-            count++
+            cnt++
             if(num == 1){
-                return count
+                return cnt
             }
         }
     }
