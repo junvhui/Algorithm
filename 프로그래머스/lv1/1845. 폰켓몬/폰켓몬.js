896928fb9a3358320382da1a9816c1fa62428408
Junhui Lee <dev.junhui@gmail.com>
2023-04-25 00:15:09 +0900

    [level 1] Title: 폰켓몬, Time: 0.81 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,13 @@
 function solution(nums) {
-    var answer = [];
+    var result = [];
     var max = nums.length / 2;
     
     for(let i = 0; i < nums.length; i++){
-        if(answer.length < max){
-            if(!answer.includes(nums[i])){
-                answer.push(nums[i])
+        if(result.length < max){
+            if(!result.includes(nums[i])){
+                result.push(nums[i])
             }
         }
     }
-    return answer.length;
+    return result.length;
 }
