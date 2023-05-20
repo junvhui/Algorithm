01007ad418f572988ba37e3dc41c4f0d37d88bf2
Junhui Lee <dev.junhui@gmail.com>
2023-05-20 15:42:53 +0900

    [D2] Title: 시각 덧셈, Time: 161 ms, Memory: 58,508 KB -BaekjoonHub

---
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
@@ -0,0 +1,15 @@
+
+T = int(input())
+for test_case in range(1, T + 1):
+	h1, m1, h2, m2 = map(int, input().split())
+    
+	hour = h1 + h2
+	minute = m1 + m2
+    
+	if hour > 12:
+		hour -= 12
+	if minute >= 60:
+		hour += 1		
+		minute -= 60
+        
+	print(f'#{test_case} {hour} {minute}')
