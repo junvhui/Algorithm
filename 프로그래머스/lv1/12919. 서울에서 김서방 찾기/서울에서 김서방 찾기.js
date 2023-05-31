4cea4025f424358c099d43941bf2bd4484ad6b9c
Junhui Lee <dev.junhui@gmail.com>
2023-05-31 23:27:08 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,5 +1,5 @@
 function solution(seoul) {
-    var answer = '';
+    var ans = '';
     for(let i = 0; i < seoul.length; i++){
         if(seoul[i] == "Kim"){
             return "김서방은 "+ i + "에 있다"
