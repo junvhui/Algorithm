dfbcb01f08ffc2b545687e49b260269ff8bb506f
Junhui Lee <dev.junhui@gmail.com>
2023-06-23 02:04:04 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,7 +1,11 @@
 function solution(n) {
     var answer = '';
     for(let i = 1; i <= n; i++){
-        answer += (i%2!==0) ? "수" : "박"
+        if(i % 2 !== 0){
+            answer += "수"
+        }else{
+            answer += "박"
+        }
     }
     return answer;
 }
