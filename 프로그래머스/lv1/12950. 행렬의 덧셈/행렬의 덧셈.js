8b5661776c2a2c61e5a58b02a952dd6493b1f64b
Junhui Lee <dev.junhui@gmail.com>
2023-06-27 00:51:48 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.11 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,10 +1,11 @@
 function solution(arr1, arr2) {
-    let answer = [[]];
+    var answer = [];
     for(let i = 0; i < arr1.length; i++){
-        answer[i] = [];
-        for(let j = 0; j < arr1[0].length; j++){
-            answer[i].push(arr1[i][j] + arr2[i][j])
+        let sum = [];
+        for(let j = 0 ; j < arr1[i].length; j++){
+            sum.push(arr1[i][j]+arr2[i][j]);
         }
+        answer.push(sum)
     }
     return answer;
 }
