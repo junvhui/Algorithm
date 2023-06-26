36dfdf51f703af7d403676f74d02a639c9e6732c
Junhui Lee <dev.junhui@gmail.com>
2023-06-27 00:39:28 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,10 +1,16 @@
 function solution(s) {
-
-  if (s.length !== 4 && s.length !== 6) return false;
-
-  for (let i = 0; i < s.length; i++) {
-    if (isNaN(Number(s[i]))) return false;
-  }
-
-  return true;
+    var answer = true;
+    
+    if(s.length === 4 || s.length === 6){
+        for(let i = 0; i < s.length; i++){
+            if(isNaN(s[i])){
+                return false;
+            }else{
+                answer = true;
+            }
+        }
+        return answer;
+    }else{
+        return false;
+    }
 }
