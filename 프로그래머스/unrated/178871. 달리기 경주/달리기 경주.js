71d7837cb80220e099741125c9f4023c7e7a1cf6
Junhui Lee <dev.junhui@gmail.com>
2023-06-28 22:40:15 +0900

    [unrated] Title: 달리기 경주, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,20 +1,43 @@
 function solution(players, callings) {
-    var runner ={};             //obj생성
+    //obj생성
+    var runner ={};             
     players.map((p,index)=>{
         runner[p] = index;
     });
     
-    for (let i = 0; i < callings.length; i++) {
-        const callName = runner[callings[i]];
-        const overtakedName = players[callName-1];
+    for(let i = 0; i < callings.length; i++) {
+        const calledName = runner[callings[i]];
+        const overtakedName = players[calledName-1];
 
         //players 순서에 값 넣기 
-        players[callName-1] = callings[i];
-        players[callName] = overtakedName;
+        players[calledName-1] = callings[i];
+        players[calledName] = overtakedName;
 
         //obj 값변경
-        runner[callings[i]] = callName-1;
-        runner[overtakedName]  = callName;
+        runner[callings[i]] = calledName-1;
+        runner[overtakedName]  = calledName;
+
     }
     return players;
 }
+
+
+
+
+
+
+/*
+runner obj 생성해서 
+mumu : 0 , soe : 1 , poe : 2, kai : 3, mine : 4 로 만듦
+
+해설진에 불린 선수 : calledName = runner[calling[i]] 를 넣음 // calledName = 3
+추월당한 선수 : overtakedName = players[calledName-1]; // 불린 선수보다 인덱스 1개 앞섬
+
+해설진에 불린 선수를 players[calledName-1] 에 넣음
+원래자리에는 추월당한 선수 넣음
+
+runner obj의 값도 바꿔줘야하므로 runner[calling[i]] = calledName - 1;
+추월당한 선수는 runner[overtakedName] = calledName
+
+
+*/
