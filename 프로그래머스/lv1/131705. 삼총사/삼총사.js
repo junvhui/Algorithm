c4522242e8ce0a0b7aa10196a43131b9f6b45555
Junhui Lee <dev.junhui@gmail.com>
2023-06-29 23:52:25 +0900

    [level 1] Title: 삼총사, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,11 +1,10 @@
 function solution(number) {
-    let answer = 0;
-    
+    var answer = 0;
     for(let i = 0; i < number.length; i++){
-        for(let j = i + 1; j < number.length; j++){
-            for(let k = j + 1; k < number.length; k++){
+        for(let j = i+1; j < number.length; j++){
+            for(let k = j+1; k < number.length; k++){
                 if(number[i] + number[j] + number[k] == 0){
-                    answer += 1;
+                    answer += 1
                 }
             }
         }
