756d06261f6d3a3be04b546b4169025a0cb14560
Junhui Lee <dev.junhui@gmail.com>
2023-06-30 00:25:29 +0900

    [level 1] Title: 시저 암호, Time: 5.51 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,18 +1,19 @@
 function solution(s, n) {
-    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lower = "abcdefghijklmnopqrstuvwxyz";
-    var answer= '';
-
-    for(var i =0; i <s.length; i++){
-        var text = s[i];
-        if(text == ' ') {
-            answer += ' '; 
+    var big = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var small = "abcdefghijklmnopqrstuvwxyz";
+    var answer = '';
+    
+    for(let i = 0; i < s.length; i++){
+        if(s[i] == " "){
+            answer += " ";
             continue;
         }
-        var textArr = upper.includes(text) ? upper : lower;
-        var index = textArr.indexOf(text)+n;
-        if(index >= textArr.length) index -= textArr.length;
-        answer += textArr[index];
+        var size = big.includes(s[i]) ? big : small;
+        var index = size.indexOf(s[i]) + n
+        if(index >= size.length){
+            index -= size.length;
+        }
+        answer += size[index]
     }
-    return answer;
+   return answer
 }
