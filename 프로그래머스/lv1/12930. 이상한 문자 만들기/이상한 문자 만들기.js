38f7281083909270fb6bf2b957d8c8962a7ab4eb
Junhui Lee <dev.junhui@gmail.com>
2023-07-04 21:17:58 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,9 +1,9 @@
 function solution(s) {
     var answer = '';
-    var split_s = s.split(" ")
+    var split_s = s.split(' ')
     for(let i = 0; i < split_s.length; i++){
         for(let j = 0; j < split_s[i].length; j++){
-            if(j % 2 == 0){
+            if(j % 2 === 0){
                 answer += split_s[i][j].toUpperCase()
             }else{
                 answer += split_s[i][j].toLowerCase()
