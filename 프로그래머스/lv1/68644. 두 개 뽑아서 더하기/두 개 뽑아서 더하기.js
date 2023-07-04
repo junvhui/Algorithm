3b03c604ebb672f078005afd9004246c87867496
Junhui Lee <dev.junhui@gmail.com>
2023-07-04 22:42:47 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.47 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,10 +1,11 @@
 function solution(numbers) {
     var answer = [];
     for(let i = 0; i < numbers.length; i++){
-        for(let j = i+1; j < numbers.length; j++){
-            answer.push(numbers[i]+numbers[j])
+        for(let j = i + 1; j < numbers.length; j++){
+            answer.push(numbers[i] + numbers[j])
         }
     }
-    let result = [...new Set(answer)]
-    return result.sort((a,b)=>a-b);
+    answer = [...new Set(answer)].sort((a,b)=>a-b)
+    
+    return answer;
 }
