fc841e498ca0a7543a482c422be16018e0df6fca
Junhui Lee <dev.junhui@gmail.com>
2023-07-04 20:30:10 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,13 +1,13 @@
 function solution(array, commands) {
-    let result = [];
-    let arr = [];
+    var result = [];
+    var arr = [];
     for(let i = 0; i < commands.length; i++){
         for(let j = commands[i][0]; j <= commands[i][1]; j++){
             arr.push(array[j-1])
         }
-        arr.sort((a, b) => a - b);
+        arr.sort((a,b) => a-b);
         result.push(arr[commands[i][2] - 1 ]);
         arr = [];
     }
-    return result;
+    return result
 }
