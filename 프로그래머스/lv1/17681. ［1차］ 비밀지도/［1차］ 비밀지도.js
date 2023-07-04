9af35aef20569c0fc28dc3d8845ecdc218612fa8
Junhui Lee <dev.junhui@gmail.com>
2023-07-04 22:13:35 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -2,14 +2,14 @@ function solution(n, arr1, arr2) {
     var answer = [];
     var i = 0, j = 0, q = 0;
     for(i = 0; i < n; i++){
-        answer[i]=(arr1[i] | arr2[i]).toString(2);
-        q = n - (answer[i].length);
-        for(j = 0; j < q; j++){
-            answer[i] = "0" + answer[i];
-        }
+    answer[i]=(arr1[i] | arr2[i]).toString(2);     
+    q = n - (answer[i].length);
+    for(j = 0; j < q; j++){
+        answer[i] = "0" + answer[i];
+    }
     }
 
-    for(i=0;i<n;i++){
+                for(i = 0; i < n; i++){
         answer[i]=answer[i].replace(/1/g,"#").replace(/0/g," ");
     }
     return answer;
