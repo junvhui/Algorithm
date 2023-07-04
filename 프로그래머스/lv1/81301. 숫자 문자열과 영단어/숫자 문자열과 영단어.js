4edd31e1d3bab6ba00e194904e686bf8145b4622
Junhui Lee <dev.junhui@gmail.com>
2023-07-04 17:35:28 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,10 +1,11 @@
 function solution(s) {
-    let number = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    let answer = s;
-    for(let i = 0; i < number.length; i++){
-        let arr = answer.split(number[i])
-        answer = arr.join(i)
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    for(let i = 0; i < numbers.length; i++) {
+        let arr = s.split(numbers[i]);
+        s = arr.join(i);
     }
     
-    return parseInt(answer);
+    return parseInt(s)
+
 }
