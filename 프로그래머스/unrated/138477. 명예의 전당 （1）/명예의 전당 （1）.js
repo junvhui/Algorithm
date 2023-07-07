554a4aebd7ec22be5e4519a9b4bd7fc8d8508a51
Junhui Lee <dev.junhui@gmail.com>
2023-07-07 17:20:42 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,10 +1,18 @@
 function solution(k, score) {
     var answer = [];
-    var nums = [];
-    score.forEach(num => {
-        nums.push(num);
-        nums.sort((a, b) => b-a).splice(k);
-        answer.push(Math.min.apply(null, nums));
-    })
-    return answer;
+    var result = [];
+    for(let i = 0; i < score.length; i++){
+        if(answer.length < k){
+            answer.push(score[i])
+            answer.sort((a, b) => a - b)   
+            result.push(answer[0])
+        }else{
+            answer.push(score[i])
+            answer.sort((a, b) => a - b)
+            answer.shift()
+            result.push(answer[0])
+            
+        }
+    }
+    return result;
 }
