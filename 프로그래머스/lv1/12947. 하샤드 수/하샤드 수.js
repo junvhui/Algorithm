34762b7367ac7184c3023262741e803637287ea8
Junhui Lee <dev.junhui@gmail.com>
2023-07-13 22:51:58 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,7 +1,7 @@
 function solution(x) {
     
-    let s = x.toString().split("")
-    let n = 0;
+    var s = x.toString().split("")
+    var n = 0;
     
     for(let i = 0; i < s.length; i++){
         n += parseInt(s[i]);
