76726d8214397f07439911762f2dc8553680b3ce
Junhui Lee <dev.junhui@gmail.com>
2023-07-14 23:07:54 +0900

    [unrated] Title: 기사단원의 무기, Time: 4.12 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,31 +1,20 @@
 function solution(number, limit, power) {
-    var div = 0;
-    var div_count = 0;
+    let div = 0;
+    let div_count = 0;
     for(let i = 1; i <= number; i++){
         div = 0;
-        for(let j = 1; j <= i/2; j++){
+        for(let j = 1; j <= i/2; j++){      //약수는 나누는 수의 절반을 넘을 수 없으므로 i/2
             if(i % j === 0){
                 div += 1
             }
         }
         div += 1
         div > limit ? div_count += power : div_count += div
+        // 만약 div > limit이면 power 값 더하고 아니면 div값 더함
     }
     
     return div_count;
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
