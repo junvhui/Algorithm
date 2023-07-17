87e651a8035d69af0bb1701cee2bcd6aa7af0218
Junhui Lee <dev.junhui@gmail.com>
2023-07-17 21:06:16 +0900

    [unrated] Title: 덧칠하기, Time: 1.92 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,6 +1,6 @@
 function solution(n, m, section) {
-    let answer = 0;
-    let painted = 0;
+    var answer = 0;
+    var painted = 0;
     
     for(let i = 0; i < section.length; i++) {
         if(painted < section[i]) {
