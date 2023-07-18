264ac78950232f1c43aa8296a207ba1ed13acbc6
Junhui Lee <dev.junhui@gmail.com>
2023-07-18 23:03:15 +0900

    [unrated] Title: 둘만의 암호, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -1,13 +1,29 @@
+function solution(s, skip, index) {
+    var answer = '';
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    var alphabet_filter = [...alphabet].filter(v => ![...skip].includes(v));
+    var s_arr = [...s]
+    for(let i = 0; i < s_arr.length; i++){
+        answer += alphabet_filter[(alphabet_filter.indexOf(s_arr[i]) + index) % alphabet_filter.length];
+    }
+    return answer
+}
 
+/*
+a-z 까지 담긴 배열에서 filter를 사용해 skip이 포함하고 있는 v를 필터링함
+그리고 answer에는 alphabet에서의 s_arr의 인덱스에 index를 더한 값에 해당하는 글자를 추가 => alphabet 인덱스를 넘을 경우 % alphabet.length를 사용함
+*/
 
-
+/* 실패 코드
 function solution(s, skip, index) {
     var answer = '';
     var alphabet = [...'abcdefghijklmnopqrstuvwxyz'].filter(v => ![...skip].includes(v));
     let s_arr = [...s]
     for(let i = 0; i < s_arr.length; i++){
-        answer += alphabet[(alphabet.indexOf(s_arr[i]) + index) % alphabet.length];
+        let idx = alphabet.indexOf(s_arr[i])
+        let new_idx = idx + index > alphabet.length - 1 ? idx +index - alphabet.length : idx + index;
+        answer += alphabet[new_idx]
     }
     return answer
 }
-
+*/
