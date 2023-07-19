224705980233a9c41db938ef0e91321809e5e10b
Junhui Lee <dev.junhui@gmail.com>
2023-07-19 21:01:41 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,8 +1,8 @@
 function solution(a, b, n) {
-    var answer = 0;
+    let answer = 0;
     while(a <= n){
-        answer += parseInt(n/a)*b
-        n = parseInt(n/a)*b + parseInt(n%a)
+        answer += parseInt(n/a)*b       // n(가지고 있는 빈병) / a * b(가져다주면 주는 콜라 병 수)
+        n = parseInt(n/a)*b + parseInt(n%a) // n을 위에서 받은 콜라에 아까 남은 콜라 더한 값으로 업데이트
     }
     return answer;
 }
