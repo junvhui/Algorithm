ecf263496ab5aa211b0cb8f82019db4fd9dbd744
Junhui Lee <dev.junhui@gmail.com>
2023-07-25 17:10:08 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,23 +1,21 @@
 function solution(lottos, win_nums) {
     var answer = [];
-    var rank = {0 : 6, 1 : 6, 2 : 5, 3 : 4, 4 : 3, 5 : 2, 6 : 1}
+    var rank = {0 : 6, 1 : 6, 2 : 5, 3 : 4, 4 : 3, 5 : 2, 6 : 1}   // 순위 지정
     let count = 0;
     
     for(let i = 0; i < lottos.length; i++){
         for(let j = 0; j < win_nums.length; j++){
-            if(lottos[i] == win_nums[j]){
+            if(lottos[i] == win_nums[j]){                   // 같으면 카운트 추가
                 count += 1;
             }
         }
     }
-    if(count == null){
-        count = 6
-    }
-    answer.push(rank[count])
+
+    answer.push(rank[count])                            //최저순위 
     
     for(let k = 0; k < lottos.length; k++){
-        if(lottos[k] == 0){
-            count += 1;
+        if(lottos[k] == 0){                             // 0인 경우가 다 당첨이라고 가정
+            count += 1;                                 // 최고순위
         }
     }
     answer.push(rank[count])
