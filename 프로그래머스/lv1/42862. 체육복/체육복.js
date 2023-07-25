bd4ed96c4ece8712a014198bd8815248c48ea024
Junhui Lee <dev.junhui@gmail.com>
2023-07-25 20:28:01 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,28 +1,24 @@
 function solution(n, lost, reserve) {
-    let lostLength = lost.length;
-    let answer =  n - lostLength;
-    let tmp = [];
-    
-    // 체육복도 잃어버리고 여분의 체육복도 동시에 있는 경우
-    for (let i = 0; i < lostLength ; i ++) {
-        if (reserve.includes(lost[i])) {
-            reserve = reserve.filter((ele) => ele !== lost[i])
-            answer += 1;
-        } else {
-            tmp.push(lost[i]);
+    const students = {};
+    let answer = 0;
+    for(let i = 1; i <= n; i++){
+        students[i] = 1;
+    }
+    lost.forEach(number => students[number] -= 1);
+    reserve.forEach(number => students[number] += 1);
+
+    for(let i = 1; i <= n; i++){
+        if(students[i] === 2 && students[i-1] === 0){
+                students[i-1]++;
+                students[i]--;
+        } else if(students[i] === 2 && students[i+1] === 0){
+                students[i+1]++;
+                students[i]--;
         }
     }
-    // 순서에 상관없이 들어올 입력을 대비해 정렬해줌
-    lost = tmp.sort();
-    
-    for (let i = 0 ; i < lostLength ; i ++) {
-        if (reserve.includes(lost[i] - 1 )) {
-            answer += 1 ;
-            reserve = reserve.filter ((ele) => ele !== lost[i] - 1 )
-        }
-        else if (reserve.includes(lost[i] + 1)) {
-            answer += 1;
-            reserve = reserve.filter((ele) => ele !== lost[i] + 1)
+    for(let key in students){
+        if(students[key] >= 1){
+            answer++;
         }
     }
     return answer;
