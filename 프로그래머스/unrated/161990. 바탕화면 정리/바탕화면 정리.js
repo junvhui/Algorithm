358bcbbfb316b11034984dee379f96a99738f301
Junhui Lee <dev.junhui@gmail.com>
2023-08-02 23:25:59 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,26 +1,18 @@
 function solution(wallpaper) {
-    var min_i = 51;
-    var min_j = 51;
-    var max_i = 0;
-    var max_j = 0;
+    let min_i = 51;
+    let min_j = 51;
+    let max_i = 0;
+    let max_j = 0;
     for(let i = 0; i < wallpaper.length; i++){
         for(let j = 0; j < wallpaper[0].length; j++){
             if(wallpaper[i][j] == "#"){
-                if(max_i < i){
-                    max_i = i
-                }
-                if(max_j < j){
-                     max_j = j
-                }
-                if(min_i > i){
-                    min_i = i
-                }
-                if(min_j > j){
-                    min_j = j
-                }
+                max_i = Math.max(max_i, i);
+                max_j = Math.max(max_j, j);
+                min_i = Math.min(min_i, i);
+                min_j = Math.min(min_j, j);
+            }
                 
             }
         }
-    }
     return [min_i, min_j, max_i+1, max_j+1];
 }
