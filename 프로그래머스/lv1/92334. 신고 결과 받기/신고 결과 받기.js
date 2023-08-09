f009af9545da2a998176168ec1ee1cd7e529c944
Junhui Lee <dev.junhui@gmail.com>
2023-08-09 22:28:39 +0900

    [level 1] Title: 신고 결과 받기, Time: 3.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,7 +1,8 @@
 function solution(id_list, report, k) {
-    let answer = new Array(id_list.length);
-    answer.fill(0);
-    let setReport = [...new Set(report)];
+    let answer = new Array(id_list.length);     // id_list 길이만큼 새로운 배열 생성
+    answer.fill(0);                             // [0, 0, 0, 0]
+
+    let setReport = [...new Set(report)];       // report 중복제거
     let arr = [];
     let user = [];
     let reportingUser = [];
@@ -10,10 +11,15 @@ function solution(id_list, report, k) {
     for (let i = 0; i < setReport.length; i++) {
         arr.push(setReport[i].split(' '));
     }
+    console.log(arr)
+
+
 
     for (let i = 0; i < id_list.length; i++) {
         user.push({ name: id_list[i], reported: 0 });
     }
+    // 신고자의 신고횟수 : 0 으로 세팅
+    // user 는 신고한 유저
 
     for (let i = 0; i < user.length; i++) {
         for (let j = 0; j < arr.length; j++) {
