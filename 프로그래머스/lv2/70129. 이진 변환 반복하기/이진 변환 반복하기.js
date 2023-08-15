a5b48793c3af4d34412cba5c375baf7b9c67aa1f
Junhui Lee <dev.junhui@gmail.com>
2023-08-15 14:35:45 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.22 ms, Memory: 35.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  let answer = [0, 0];
+
+
+  while (s.length > 1) {
+    let sLength = s.length;
+    s = s.split("0").join("");
+    answer[0] += 1
+    answer[1] += (sLength - s.length);      // 원래 길이에서 0이 없어진 길이만큼 빼서 더해줌
+    s = s.length.toString(2);
+  }
+
+  return answer;
+}
