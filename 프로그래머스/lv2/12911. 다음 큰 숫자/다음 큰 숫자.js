dc2320fc5b941f8ee7dd495824eefd351f0e0522
Junhui Lee <dev.junhui@gmail.com>
2023-08-16 21:54:57 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,10 +1,10 @@
 function solution(n) {
-    let arr = n.toString(2).split(''); 
-    let count = arr.filter((one)=>one === "1").length
+    let arr = n.toString(2).split('');                  // 2진수로 만듦
+    let count = arr.filter((one)=>one === "1").length   // 2진수에서 1만 냅두게 필터링
     let count2;
-    while(count !== count2){
+    while(count !== count2){                            // 원래 1의 개수랑 다음 숫자의 1개수가 다르면 계속 진행
         n++
-        count2 = n.toString(2).split('').filter((one)=>one === "1").length;
+        count2 = n.toString(2).split('').filter((one)=>one === "1").length; // 다음 숫자의 1의 개수 찾기
     }
-    return n;
+    return n;                                           // 같으면 끝나므로 n 출력
 }
