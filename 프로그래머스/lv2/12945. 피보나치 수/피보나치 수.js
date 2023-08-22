aca88972157003b7908f14a4e0348c0f3207bd3f
Junhui Lee <dev.junhui@gmail.com>
2023-08-22 23:35:10 +0900

    [level 2] Title: 피보나치 수, Time: 4.53 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,17 +1,17 @@
 function solution(n) {
-    let answer = [];
+    var answer = [];
     for(let i =0; i <= n;i++){
-        if (i === 0){
+        if (i === 0){                           // 0이면 0넣고
             answer.push(0)
         }
-        if (i === 1){
+        if (i === 1){                           // 1이면 1넣고
             answer.push(1)
         }
-        if (i >=2){
-            let sum = answer[i-2] + answer[i-1];
-            answer.push(sum % 1234567)
+        if (i >=2){                                 // 2이상 부터는
+            let sum = answer[i-2] + answer[i-1];    // 이전 값 더해서 
+            answer.push(sum % 1234567)              // 그 값을 1234567로 나눈 값을 넣고
         }
     }
-    let result = answer[n] % 1234567
+    let result = answer[n] % 1234567                // n번째 값 출력
     return result;
 }
