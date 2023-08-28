72e52f6847d018d3bc1be4236a48d69aad7488d0
Junhui Lee <dev.junhui@gmail.com>
2023-08-28 12:00:33 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,28 +1,29 @@
 function solution(n, words) {
-    var stack = [];
+    var stack = [];                     // 글자 넣어줄 스택
     var answer = [0, 0];
     
     for(let i = 0; i < words.length; i++){
-        if(stack.length === 0){
-            stack.push(words[i]);
+        if(stack.length === 0){         // 만약 스택이 비었다면
+            stack.push(words[i]);       // 글자 넣고
             continue;
         }
-        var pre = stack[stack.length - 1];
-        var preWord = pre[pre.length - 1];
+        var pre = stack[stack.length - 1];  // 이전 단어
+        var preWord = pre[pre.length - 1];  // 이전 단어의 마지막 글자
         
-        var now = words[i];
-        var nowWord = now[0];
+        var now = words[i];                 // 현재 단어
+        var nowWord = now[0];               // 현재 단어의 첫 글자
         
-        if(preWord !== nowWord || stack.includes(now)){
-            var x = (i % n) + 1;
-            var y = Math.ceil((i + 1) / n);
+        if(preWord !== nowWord || stack.includes(now)){     // 이전 단어의 마지막 글자와 현재 단어의 첫 글자가 다르거나, 
+                                                            // 스택에 현재 단어가 있으면
+            var x = (i % n) + 1;                            // 몇 번째 사람 x는 i를 사람 수로 나눈 + 1 값 
+            var y = Math.ceil((i + 1) / n);                 // 몇 번째 단어 y는 i+1 을 사람 수로 나눈 올림 값
             
             answer[0] = x;
             answer[1] = y;
             break;
         }
         
-        stack.push(words[i]);
+        stack.push(words[i]);                           // 해당사항 없으면 스택에 단어 넣기
     }
     
     return answer;
