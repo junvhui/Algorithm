04d463b74266441f9eb01fe4035afa355ea6ba21
Junhui Lee <dev.junhui@gmail.com>
2023-08-31 17:56:39 +0900

    [Silver IV] Title: 30, Time: 212 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.js
+++ 백준/Silver/10610. 30/30.js
@@ -1,5 +1,5 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().trim();
 const numArr = input.split("").map(Number);
-numArr.sort((a, b) => b - a);
+numArr.sort((a, b) => b - a);    //내림차순 정렬
 const sum = numArr.reduce((acc, cur) => acc + cur);
 console.log(sum % 3 === 0 && numArr.includes(0) ? numArr.join("") : -1);
