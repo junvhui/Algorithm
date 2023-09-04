465f1c5f5ea3045c1e4f171fe4ad71243ab4706a
Junhui Lee <dev.junhui@gmail.com>
2023-09-04 15:04:02 +0900

    [Silver IV] Title: 대칭 차집합, Time: 340 ms, Memory: 68608 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.js
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.js
@@ -1,14 +1,18 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
-let [N,M] = [input[0].split(' ')[0],input[0].split(' ')[1]]
-let arr1 = input[1].split(' ').map(Number)
-let arr2 = input[2].split(' ').map(Number)
+const [n, input1, input2] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+//제출 할때 /dev/stdin으로 수정
 
-function subtraction(a,b){
-    let set1 = new Set(a)
-    let set2 = new Set(b)    
-    let aSubtraction = new Set([...set1].filter((x)=>!set2.has(x))).size
-    let bSubtraction = new Set([...set2].filter((x)=>!set1.has(x))).size
-    console.log(aSubtraction+bSubtraction)
-}
+//a 원소의 개수, b 원소의 개수
+//a 원소들
+//b 원소들
+// const a = n.split(" ")[0];
+// const b = n.split(" ")[1];
+const aArr = input1.split(" ");
+const bArr = input2.split(" ");
 
-subtraction(arr1,arr2)
+const arr = []
+const aaa = arr.concat(aArr, bArr)
+const vvv = new Set(aaa)
+
+const num = aaa.length - vvv.size
+
+console.log((aArr.length-num) + (bArr.length-num))
