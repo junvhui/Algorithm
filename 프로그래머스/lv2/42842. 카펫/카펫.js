aaea2a8404433d97ee58faaf0b38dedb5e2020d3
Junhui Lee <dev.junhui@gmail.com>
2023-10-05 22:26:46 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,5 +1,5 @@
 function solution(brown, yellow) {
-    var answer = [];
+
     for (var y = 3; y <= (brown+yellow)/y; y++) {   // y는 무조건 3 이상이고 
         var x = Math.floor((brown+yellow)/y);       // x는 전체 개수에서 y 개수를 나눈 값
         if((x-2)*(y-2)=== yellow) {                 // 만약 x-2 * y-2 가 yellow 값이라면
