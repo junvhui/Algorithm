491eac31c4e7b612bd0576048ad6475a820792a6
Junhui Lee <dev.junhui@gmail.com>
2023-10-11 23:47:53 +0900

    [level 2] Title: 할인 행사, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,27 +1,28 @@
 function solution(want, number, discount) {
 
-    var result = 0;
+    let result = 0;
     
-    discount.forEach((v, i) => {
+    discount.forEach((v, i) => {                        // discount 첫번째 부터 돌림
         
-        var discounts = [...discount].slice(i, i+10) 
+        var discounts = [...discount].slice(i, i+10)    // discount의 돌리는 인덱스부터 +10 값들을 
+                                                        // discounts에 넣음
        
-        if(discounts < 10){
-            return result;
+        if(discounts < 10){                             // discounts가 10보다 작으면 이미 10일 연속은 불가하므로
+            return result;                              // 종료
         }
         
-        let check = 0;
+        let check = 0;                                  
         
-        for(let j = 0; j < want.length; j++){
+        for(let j = 0; j < want.length; j++){           // discounts 필터링해서 want의 값 개수랑 같으면
             if([...discounts].filter((v) => v == want[j]).length === number[j]){
-                check ++;
+                check ++;                               // 할인 받을 수 있는 상품이므로 +1
             }else{
-                break;
+                break;                                  // 아니면 그냥 종료, 이미 실패
             }
         }
         
-        if(check === want.length){
-            result ++;
+        if(check === want.length){                      // for문 다 돌리고나서 check 값과 want의 길이가 같다면
+            result ++;                                  // 모든 상품들 할인 받을 수 있으므로 result에 1 추가
         }
         
     })
