38674665047383bf31908d2a7abee66d6ee55b75
Junhui Lee <dev.junhui@gmail.com>
2023-11-11 23:38:19 +0900

    [D2] Title: 지그재그 숫자, Time: 141 ms, Memory: 58,528 KB -BaekjoonHub

--- SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
@@ -1,6 +1,6 @@
 T = int(input())
 
-for test_case in range(1, T+1):
+for tc in range(1, T+1):
     n = int(input())
     total = 0
     for i in range(1, n+1):
@@ -8,4 +8,4 @@ for test_case in range(1, T+1):
         	total -= i
     	else:
         	total += i
-    print(f"#{test_case} {total}")
+    print(f"#{tc} {total}")
