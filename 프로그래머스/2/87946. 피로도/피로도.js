dcff02302b98eb378510435a71ccc50b7d6935ae
Junhui Lee <dev.junhui@gmail.com>
2023-11-20 23:54:39 +0900

    [level 2] Title: 피로도, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,12 +1,13 @@
 
 function solution(k, dungeons) {
-   let answer = 0;
-   // 각 던전을 체크하는 배열
+   var answer = 0;
+    // 각 던전을 체크하는 배열
    const checkedArray = new Array(dungeons.length).fill(false);
+    // [false, false, false]
    
     // 깊이 우선 탐색 함수 선언
 function DFS(k, dungeons, count, checkedArray) {
-   // 모든 경우의 수를 순회
+    // 모든 경우의 수를 순회
    for (let i = 0; i < dungeons.length; i++) {
        // 방문하지 않았고 현재 피로도 k로 접근 가능한 곳을 접근
        if (!checkedArray[i] && k >= dungeons[i][0]) {
@@ -19,7 +20,7 @@ function DFS(k, dungeons, count, checkedArray) {
       }
   }
    // 최댓값을 구하기
-   answer = answer < count ? count : answer;
+   answer = Math.max(answer, count);
 }
    DFS(k, dungeons, 0, checkedArray)
    
