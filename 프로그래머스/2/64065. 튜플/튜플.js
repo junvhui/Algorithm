c3736138c174d6fd56419b371158308f6ecffd1a
Junhui Lee <dev.junhui@gmail.com>
2023-11-21 23:54:34 +0900

    [level 2] Title: 튜플, Time: 0.09 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,14 +1,19 @@
 function solution(s) {
-    var answer = [];
+    let answer = [];
     s.substring(2, s.length - 2)                             // 문자열로 바꿔줌
      .split("},{")                                           // '{' '}' 없애기
      .map((e) => e.split(",").map((e) => parseInt(e)))       // 숫자로 변경
      .sort((a, b) => a.length - b.length)
-     .forEach((arr) => {  
+     .forEach((arr) => {                                     
         arr.forEach((v) => {
-            if (!answer.includes(v)) answer.push(v);
+            if (!answer.includes(v)) {                       // 각각에서 answer에 없는 값들 추가
+                answer.push(v);
+            }
         });
     });
     
     return answer;
 }
+
+
+// 보고품
