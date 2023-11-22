19c6e79be64b1d73481026eb22fbe73dc5cfde0e
Junhui Lee <dev.junhui@gmail.com>
2023-11-22 17:44:08 +0900

    [level 2] Title: 전화번호 목록, Time: 521.79 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,17 +1,20 @@
 function solution(phone_book) {
+    
     const map = new Map();
 
     for (let i = 0; i < phone_book.length; i++) {
-        map.set(phone_book[i], 1);
+        map.set(phone_book[i], 1);                              // '123'=> 1, '456' => 1, '789' => 1
     }
 
-    for (let i = 0; i < phone_book.length; i++) {
-        for (let j = 0; j < phone_book[i].length; j++) {
-            if (map.has(phone_book[i].substring(0, j))) {
-                return false;
+    for (let i = 0; i < phone_book.length; i++) {               
+        for (let j = 0; j < phone_book[i].length; j++) {        // 길이만큼
+            if (map.has(phone_book[i].substring(0, j))) {       // 만약 map 값 중에 substring 한 값을 가진게 존재하면
+                return false;                                   // false
             }
         }
     }
 
     return true;
 }
+
+// 
