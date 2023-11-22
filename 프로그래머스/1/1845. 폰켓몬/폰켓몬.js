d90c5db232dc5290b7b1b140a0393f96f6f0a29a
Junhui Lee <dev.junhui@gmail.com>
2023-11-22 17:23:12 +0900

    [level 1] Title: 폰켓몬, Time: 2.81 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -6,12 +6,20 @@ function solution(nums) {
         pocketMons[pocketmon] = pocketMons[pocketmon] + 1 || 1;
 
     })
+
     
-    if(Object.keys(pocketMons).length >= nums.length / 2 ){
-        result = nums.length / 2
-    }else{
-        result = Object.keys(pocketMons).length
-    }
-    
-    return result
+    return Object.keys(pocketMons).length > nums.length / 2 ? nums.length / 2 : Object.keys(pocketMons).length
 }
+
+
+
+/* Set 사용한 풀이 (Set 까먹고 있었음 ㅠㅠ)
+function solution(nums) {
+    let answer = 0;
+    
+    let myBag = [...new Set(nums)];
+    let limit = nums.length / 2;
+    
+    return myBag.length > limit ? limit : myBag.length;
+}
+*/
