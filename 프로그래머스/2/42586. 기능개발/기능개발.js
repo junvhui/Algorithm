e7ebbea3a07083f771495c07283fc99bcdb5dc5d
Junhui Lee <dev.junhui@gmail.com>
2023-11-24 01:02:41 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,8 +1,8 @@
 function solution(progresses, speeds) {
     var answer = [];
-    let days = 1;
-    let count = 0;
-    let progress = 0;
+    var days = 1;
+    var count = 0;
+    var progress = 0;
     
     while(progresses[0]){
         progress = progresses[0] + (speeds[0] * days);
