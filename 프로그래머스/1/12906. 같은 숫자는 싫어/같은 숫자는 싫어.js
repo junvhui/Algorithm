154eeb387ef9295cd06f60bd4d08f4e8e29fe664
Junhui Lee <dev.junhui@gmail.com>
2023-11-23 23:21:41 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.41 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,12 @@
+function solution(arr)
+{
+    var stack = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] != arr[i+1]){
+            stack.push(arr[i])
+        }
+    }
+    
+    return stack;
+}
