2c3352ae4335267a4c0af60c0df25558768b73ea
Junhui Lee <dev.junhui@gmail.com>
2023-11-24 17:00:33 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,4 +1,28 @@
 function solution(progresses, speeds) {
+    var answer = [];                                                // 결과값 배열
+    var stack = [];                                                 // 작업 과정 스택
+    
+    for(let i = 0; i < progresses.length; i++){
+        var complete = Math.ceil((100 - progresses[i]) / speeds[i]); // 100에서 progress 뺀 값에서 스피드로 나눔
+        var lastCompleted = stack[stack.length-1];                  // 가장 최근 완료한 작업
+        
+        if(stack.length > 0 && lastCompleted.complete >= complete){  // 작업한 내용이 존재하면서 가장 최근 완료한 작업의 소요 일수가 
+                                                                    // 현재 진행중인 작업의 진행 일수보다 크다면
+            lastCompleted.count++;                                  // 가장 최근 완료한 작업의 count만 1 증가(stack에 값을 새로 추가하는 것은 없음)
+        }else{                                                      // 반대로 작업 내용 없거나, 현재 진행중인 작업의 진행 일수가 크면
+            stack.push({ complete, count: 1})                       // stack에 추가
+        }
+        
+    }
+    
+    answer = stack.map((v) => v.count);                             // {7 , count : 1} 같은 형태로 담겨있으므로 count 값만 뺌
+    
+    return answer;
+}
+
+
+/*
+function solution(progresses, speeds) {
     var answer = [];
     var days = 1;
     var count = 0;
@@ -24,3 +48,4 @@ function solution(progresses, speeds) {
     
     return answer;
 }
+*/
