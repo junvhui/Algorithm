46aeb8829c0d60839ff23d01789751720ccf625f
Junhui Lee <dev.junhui@gmail.com>
2023-11-27 18:23:36 +0900

    [level 2] Title: 올바른 괄호, Time: 2.44 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,5 +1,28 @@
 function solution(s){
+    var answer = true;
     var count = 0;
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === "("){
+            count++;
+        }else{
+            count--;
+        }
+        
+        if(count < 0){
+                return false;
+            }
+    }
+    
+     return count === 0 ? true : false   // 합이 0인지 아닌지
+}
+
+
+
+
+/*
+
+function solution(s){
+    let count = 0;
 
     for(let i = 0; i < s.length; i++){
         if(s[i] === "("){
@@ -15,3 +38,4 @@ function solution(s){
 
 
 }
+*/
