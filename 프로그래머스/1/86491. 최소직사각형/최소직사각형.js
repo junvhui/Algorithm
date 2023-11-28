e0f4b0af452f78291055091d72d3baff0b1f7d5c
Junhui Lee <dev.junhui@gmail.com>
2023-11-29 01:57:14 +0900

    [level 1] Title: 최소직사각형, Time: 101.53 ms, Memory: 43.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,43 @@
+function solution(sizes) {
+    var width = 0;
+    var height = 0;
+    for(let i = 0; i < sizes.length; i++){
+        let [a, b] = sizes[i].sort((a, b) => a - b)
+        console.log([a, b])
+        if(a > width){
+            width = a;
+        }
+        
+        if(b > height){
+            height = b;
+        }
+    }
+
+    
+    return width * height;
+}
+
+
+
+
+
+
+
+/*
+function solution(sizes) {
+    var w = 0;
+    var h = 0;
+    for(let i = 0; i < sizes.length; i++){
+        let [a, b] = sizes[i].sort((a,b) => a-b);
+        if (a > h){
+            h = a
+        }
+        if (b > w){
+            w = b
+        }
+    };
+
+    return w * h;
+}
+
+*/
