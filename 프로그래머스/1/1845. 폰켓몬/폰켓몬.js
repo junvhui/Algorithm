fa679ade778b8bdd403649947f2ce0ec1b2925c9
Junhui Lee <dev.junhui@gmail.com>
2023-12-09 18:44:50 +0900

    [level 1] Title: 폰켓몬, Time: 3.34 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,14 +1,14 @@
 function solution(nums) {
-    var pocketMons = {};
-    var result = '';
+    let pocketMons = {};                                            // 포켓몬 Map 만들어줌
     
     nums.forEach((pocketmon) => {
-        pocketMons[pocketmon] = pocketMons[pocketmon] + 1 || 1;
+        pocketMons[pocketmon] = pocketMons[pocketmon] + 1 || 1;     // 포켓몬 없으면 1 할당, 있으면 +1
 
     })
 
     
     return Object.keys(pocketMons).length > nums.length / 2 ? nums.length / 2 : Object.keys(pocketMons).length
+    // 총 마리 수의 절반을 넘을 수 없으므로 절반보다 Map 객체의 키 값이 많으면 nums.length / 2 출력
 }
 
 
