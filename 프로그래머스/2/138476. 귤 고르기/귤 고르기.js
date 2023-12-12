62f4edd9ba27defce92e924f02f0e14f37e3e07d
Junhui Lee <dev.junhui@gmail.com>
2023-12-12 22:13:33 +0900

    [level 2] Title: 귤 고르기, Time: 63.98 ms, Memory: 50.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,15 +1,16 @@
 function solution(k, tangerine) {
-  let answer = 0;
+  var result = 0;
+    
   const tDict = {};
   tangerine.forEach((t) => tDict[t] = (tDict[t] || 0) + 1);     // t에 해당하는 값이 tDict 객체에 이미 존재하는지 확인하고, 만약 없다면 0을 사용하여 초기화한 뒤 1을 더함
   const tArr = Object.values(tDict).sort((a, b) => b - a);      // tArr = [2,2,2,1,1]
   for (const t of tArr) {
-        answer++;
+        result++;
         if (k > t){                                             // t가 k보다 작으면 k에서 t 빼주고 answer는 1 증가
             k -= t;
         }else{
             break;
         }
   }
-  return answer;
+  return result;
 }
