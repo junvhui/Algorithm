dad8bcea13366a963ee6b08e3797205609fc9d28
Junhui Lee <dev.junhui@gmail.com>
2023-12-17 23:54:19 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,5 +1,5 @@
 function solution(citations) {
-    var answer = 0;
+    let answer = 0;
     citations.sort((a,b)=>{
         return b-a;                     // [6, 5, 3, 1, 0]
     });                                 // [4, 3, 3, 3, 0, 0, 0] => ?
