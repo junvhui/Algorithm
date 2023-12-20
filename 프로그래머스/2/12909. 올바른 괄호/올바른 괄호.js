b76c2ca63df78a35896ad042725f32e56525d52b
Junhui Lee <dev.junhui@gmail.com>
2023-12-20 16:13:08 +0900

    [level 2] Title: 올바른 괄호, Time: 11.55 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -2,15 +2,16 @@ function solution(s){
     var stack = [];
     
     for(let i = 0; i < s.length; i++){
-        stack.push(s[i])
+        stack.push(s[i])                        // stack에 하나씩 넣을 때
         
         if(stack[stack.length - 1] === ")" && stack[stack.length - 2] === "("){
-            stack.pop();
-            stack.pop();
+                                                // 만약 넣은 값이랑 넣은 값 바로 이전 값이 "()" 이면
+            stack.pop();                        // 둘다 빼
+            stack.pop();                        // 빼
         }
     }
 
-    return stack.length === 0 ? true : false;
+    return stack.length === 0 ? true : false;  
 }
 
 
