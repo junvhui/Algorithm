1029acce283147a0f7f2ee6753bbc87b960ff131
Junhui Lee <dev.junhui@gmail.com>
2023-12-25 23:49:20 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,15 +1,15 @@
 function solution(clothes) {
-    let answer = 1;
-   	let obj={};
+    var answer = 1;
+   	var obj={};
     
-    clothes.forEach((cloth) => {
-        obj[cloth[1]]=(obj[cloth[1]] || 1) + 1;
+    clothes.forEach((cloth) => {                    // 옷 각각을 hash로 map
+        obj[cloth[1]]=(obj[cloth[1]] || 1) + 1;     // +1 은 하나의 의상만 착용하는 것이 가능해서
     })
     
-    for(let key in obj){
+    for(let key in obj){                            // obj 값 돌면서 값들을 곱해줌
         answer *= obj[key];
     }
     
-    return answer-1;
+    return answer-1;                                // 옷을 안입는 경우 : -1 해줌
     
 }
