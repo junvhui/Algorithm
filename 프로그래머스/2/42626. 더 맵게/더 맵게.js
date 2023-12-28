8409d909703743da6afb5f158fd43eee373795d3
Junhui Lee <dev.junhui@gmail.com>
2023-12-28 16:20:48 +0900

    [level 2] Title: 더 맵게, Time: 373.67 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -33,13 +33,20 @@ class MinHeap {
     this.heap[0] = this.heap.pop();
     let currentIndex = 0;
 
+    // 자식 노드가 존재한다면
     while (currentIndex * 2 + 1 < this.heap.length) {
+      // 왼쪽 자식이랑 오른쪽 자식 비교해서 더 작은 값의 인덱스 찾음
       let minChildIndex = currentIndex * 2 + 2 < this.heap.length && this.heap[currentIndex * 2 + 2] < this.heap[currentIndex * 2 + 1] ? currentIndex * 2 + 2 : currentIndex * 2 + 1;
-
+      
+        
+      // 현재 노드가 작은 자식 노드 값보다 작으면 최소 힙 충족하므로 break
       if (this.heap[currentIndex] < this.heap[minChildIndex]) {
         break;
       }
-
+      
+      // 현재 노드와 최소값을 가진 자식 노드를 교환
+      // 교환 후, 인덱스를 최소값을 가진 자식 노드의 인덱스로 갱신
+      // 이 과정을 반복하여 최소 힙의 성질을 유지
       const temp = this.heap[currentIndex];
       this.heap[currentIndex] = this.heap[minChildIndex];
       this.heap[minChildIndex] = temp;
@@ -57,6 +64,7 @@ class MinHeap {
 function solution(scoville, K) {
   const minHeap = new MinHeap();
 
+  // 스코빌 지수 오름차순 유지
   for (const sco of scoville) {
     minHeap.push(sco);
   }
@@ -71,5 +79,6 @@ function solution(scoville, K) {
     mixedCount++;
   }
 
+  // 최소 힙의값이 K보다 크거나 같으면 mixedCount 출력
   return minHeap.peek() >= K ? mixedCount : -1;
 }
