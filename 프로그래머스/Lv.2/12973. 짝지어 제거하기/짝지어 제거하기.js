b517e2a1b8c2c9b4496c2086404f0db2ccdb7dcd
Junhui Lee <dev.junhui@gmail.com>
2024-01-11 23:15:21 +0900

    [level Lv.2] Title: 짝지어 제거하기, Time: 23.48 ms, Memory: 46.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,16 @@
+function solution(s)
+{
+    let answer = [];
+    for(let i = 0; i < s.length; i++){
+        if(answer[answer.length - 1] == s[i]){              // 하나씩 추가하다가 제일 최근에 넣은 값이랑 넣으려는 값이
+            answer.pop()                                    // 같다면 빼내고
+        }else{
+            answer.push(s[i])                               // 다르면 넣음
+        }
+    }
+    if(answer.length > 0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
