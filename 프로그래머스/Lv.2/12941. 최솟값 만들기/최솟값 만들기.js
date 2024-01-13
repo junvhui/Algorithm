76d922a52f4d216fe3588c77df750b325f8138a7
Junhui Lee <dev.junhui@gmail.com>
2024-01-13 17:59:08 +0900

    [level Lv.2] Title: 최솟값 만들기, Time: 0.67 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,13 @@
+function solution(A,B){
+    let result = 0;
+
+    A = A.sort((a, b) => a-b)
+    B = B.sort((a, b) => b-a)
+    
+    for(let i = 0; i < A.length; i++){
+        result += A[i] * B[i]
+    }
+
+  
+    return result;
+}
