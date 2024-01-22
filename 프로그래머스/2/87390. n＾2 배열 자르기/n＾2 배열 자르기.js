f6bcab3ec3345c8658d0ee11762f9c8687859aa6
Junhui Lee <dev.junhui@gmail.com>
2024-01-22 23:03:38 +0900

    [level 2] Title: n^2 배열 자르기, Time: 7.67 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,5 +1,6 @@
 function solution(n, left, right) {
-let answer = [];
+    
+    let answer = [];
 
     while (left <= right) {
         answer.push(Math.max(Math.floor(left / n), left++ % n) + 1);
@@ -8,5 +9,3 @@ let answer = [];
     return answer;
 }
 
-
-// x, y 좌표 값 구해서 더 큰 값 + 1 을 넣어줌
