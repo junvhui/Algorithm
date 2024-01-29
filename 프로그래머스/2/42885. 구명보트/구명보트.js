fbb9f985b98721f9424cb629bec5aba978427b28
Junhui Lee <dev.junhui@gmail.com>
2024-01-29 23:41:18 +0900

    [level 2] Title: 구명보트, Time: 25.83 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,10 +1,8 @@
 function solution(people, limit) {
     var answer = 0;
-    
-    
     people.sort ((a,b) => b-a)  // 내림차순 정렬
     // [80,70,50,50]
-    let small = people.length - 1;
+    var small = people.length - 1;
     for (var i = 0; i <= small ; i++ ) {
         if (people[i] + people [small] <= limit ){  // 제일 큰 사람과 제일 작은 사람 더한 값이 limit 보다 작으면
             small--                                 // 제일 작은 값 --
