37f454f8373b0dc6937980b256165d6797e4bed0
Junhui Lee <dev.junhui@gmail.com>
2024-02-07 22:32:45 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1653.14 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,18 +1,18 @@
 function solution(elements) {
-    var set = new Set();
-    var len = elements.length;
+    let set = new Set();
+    let len = elements.length;
     
     for(let i = 1; i <= len; i++){
-        for(let j = 0; j < len; j++){
+        for(let j = 0; j < len; j++){       // 시작좌표
             let sum = 0;
-            for(let k = 0; k < i; k++){
+            for(let k = 0; k < i; k++){     // 시작좌표 + k
                 let index = j + k;
-                if(index >= len){
+                if(index >= len){           // index 가 길이보다 크면 길이만큼 빼줌
                     index -= len
                 }
                 sum += elements[index];
             }
-            set.add(sum)
+            set.add(sum)                    // set 사용 시 중복 자동 제거
         }
     }
     return set.size;
