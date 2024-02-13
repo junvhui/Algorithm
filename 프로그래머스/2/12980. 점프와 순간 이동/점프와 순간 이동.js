26fbeb5effd98b326456d5bc02b08e43b2aa94b2
Junhui Lee <dev.junhui@gmail.com>
2024-02-13 23:51:44 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,13 +1,13 @@
 function solution(n){
-    var answer = 0;
+var result = 0;
     while(n > 0){
         if(n % 2 === 0){    // n이 만약 2의 배수라면
             n /= 2;         // 순간이동 쓰면되니까 그냥 2로 나누어줌
         }else{              // 2의 배수가 아니라면
             n--;            // n 하나 빼주고
-            answer++           // 뺀 만큼 배터리 + 1
+            result++           // 뺀 만큼 배터리 + 1
         }
     }
 
-    return answer;
+    return result;
 }
