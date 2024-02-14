3e10fe0c42e40de98d4dceb479bd42fa3e1a5165
Junhui Lee <dev.junhui@gmail.com>
2024-02-14 23:48:38 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,12 +1,12 @@
 function solution(n,a,b)
 {
-    var result = 0;
+    let ans = 0;
 
     while(a !== b){
         a = Math.ceil(a / 2)      
         b = Math.ceil(b / 2)
-        result++;
+        ans++;
     }
 
-    return result;
+    return ans;
 }
