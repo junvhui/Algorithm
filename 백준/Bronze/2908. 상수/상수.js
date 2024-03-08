b3cc1086327d59d7cf8c20ba116dfc71d3dc9a32
Junhui Lee <dev.junhui@gmail.com>
2024-03-09 02:52:04 +0900

    [Bronze II] Title: 상수, Time: 200 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split("\n")
+
+let a = input[0].split(' ')[0];
+let b = input[0].split(' ')[1];
+
+let newA = a[2] + a[1] + a[0];
+let newB = b[2] + b[1] + b[0];
+
+
+console.log(Math.max(Number(newA), Number(newB)))
