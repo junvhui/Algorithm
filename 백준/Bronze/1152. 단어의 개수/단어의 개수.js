399769876ebd2f6427ec7bbfa652ec4bc87a489c
Junhui Lee <dev.junhui@gmail.com>
2024-03-09 15:26:26 +0900

    [Bronze II] Title: 단어의 개수, Time: 176 ms, Memory: 22656 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split("\n")
+
+
+let data = input[0].trim().split(" ");
+
+
+if(data == ""){
+  console.log(0);
+}else{
+  console.log(data.length)
+}
+
+
