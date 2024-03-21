5ab16d0bcd151646a45dd66be0970ebc0dee36f2
Junhui Lee <dev.junhui@gmail.com>
2024-03-21 17:16:49 +0900

    [Silver III] Title: 주유소, Time: 264 ms, Memory: 26372 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync('/dev/stdin').toString().split("\n")
+
+
+let N = Number(input[0]);
+let dist = input[1].split(" ").map(Number);
+let cost = input[2].split(" ").map(Number);
+
+let minCost = cost[0];
+for(let i = 0; i < N; i++){
+  minCost = Math.min(minCost, cost[i]);
+  cost[i] = minCost;
+}
+
+
+let answer = BigInt(0);
+for(let i = 0; i < N - 1; i++){
+  answer += BigInt(dist[i]) * BigInt(cost[i]);
+}
+
+console.log(parseInt(answer))
